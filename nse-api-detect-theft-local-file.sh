c69295e0c64f55c18bad20cf6ae19fb136565451
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-03-03 14:48:05 +0800

    feat: add nse theft detection utility to read local file as input
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ nse-api-detect-theft-local-file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source utils.sh
+
+setCaptureToken
+
+read -p "Asset URL: " assetFilepath
+
+echo -e "\n\nDetecting theft for asset ${assetFilepath} ...\n"
+
+curl -X POST "https://eofveg1f59hrbn.m.pipedream.net" \
+     -H "Authorization: token ${captureToken}" \
+     -F "file=@${assetFilepath}" \
+     -F "threshold=0.12" \
+     -F 'excludedAssets=["bafybeid32me6xuuamahne2vs4ks57y3wohag4dt65iwhfzpqtpw7y6f75i"]' \
+     -F 'excludedContracts=["0xb90c5b95d7c29d1448ec079dffedc5905fb77711"]'
